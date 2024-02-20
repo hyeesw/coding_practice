aa253b70634e2d71c67c70630177e06fcbff8f12
hyeesw <hyeeun7904@kookmin.ac.kr>
2024-02-20 22:45:06 +0900

    move directory from dataStructure to datastructure refer to Naver code convention

--- datastructure/bj_73017575.java
+++ datastructure/bj_73017575.java
@@ -1,4 +1,4 @@
-package dataStructure;
+package datastructure;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
-
 
 // 문제에서 요구하는 배열 size가 26으로 고정이고, 요소 추가/삽입이 없으므로 resizing이 필요 없음. => 배열이 적합
 public class bj_73017575 {
@@ -18,12 +17,12 @@ public class bj_73017575 {
 		String str = st.nextToken();
 		int[] arr = new int[26];
 
-		for(int i = 0; i < str.length(); i++) {
+		for (int i = 0; i < str.length(); i++) {
 			int idx = str.charAt(i) - 'a';
 			arr[idx]++;
 		}
 
-		for(int i=0; i<26; i++) {
+		for (int i = 0; i < 26; i++) {
 			bw.write(arr[i] + " ");
 		}
 		bw.close();
