fa37127303b317032f04fd53a74c19bd0d7aca25
hyeesw <hyeeun7904@kookmin.ac.kr>
2024-02-27 18:17:03 +0900

    10845 - Change If statement to switch-case statement

--- datastructure/bj_10845.java
+++ datastructure/bj_10845.java
@@ -11,46 +11,50 @@ public class bj_10845 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
 		int n = Integer.parseInt(br.readLine()); // 명령 개수
 		int[] queue = new int[n];
-
+		// cursor 설정
 		int fc = 0;
 		int bc = 0;
 
 		for (int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			String command = st.nextToken();
-			if (command.equals("push")) {
-				int num = Integer.parseInt(st.nextToken());
-				queue[bc] = num;
-				bc++;
-			} else if (command.equals("pop")) {
-				if (fc == bc) {
-					bw.write("-1\n");
-				} else {
-					bw.write(queue[fc] + "\n");
-					fc++;
+			switch (command) {
+				case "push" -> {
+					int num = Integer.parseInt(st.nextToken());
+					queue[bc] = num;
+					bc++;
 				}
-			} else if (command.equals("size")) {
-				bw.write(bc - fc + "\n");
-			} else if (command.equals("empty")) {
-				if (fc == bc) {
-					bw.write("1\n");
-				} else {
-					bw.write("0\n");
+				case "pop" -> {
+					if (fc == bc) {
+						bw.write("-1\n");
+					} else {
+						bw.write(queue[fc] + "\n");
+						fc++;
+					}
 				}
-			} else if (command.equals("front")) {
-				if (fc == bc) {
-					bw.write("-1\n");
-				} else {
-					bw.write(queue[fc] + "\n");
+				case "size" -> bw.write(bc - fc + "\n");
+				case "empty" -> {
+					if (fc == bc) {
+						bw.write("1\n");
+					} else {
+						bw.write("0\n");
+					}
 				}
-			} else if (command.equals("back")) {
-				if (fc == bc) {
-					bw.write("-1\n");
-				} else {
-					bw.write(queue[bc - 1] + "\n");
+				case "front" -> {
+					if (fc == bc) {
+						bw.write("-1\n");
+					} else {
+						bw.write(queue[fc] + "\n");
+					}
+				}
+				case "back" -> {
+					if (fc == bc) {
+						bw.write("-1\n");
+					} else {
+						bw.write(queue[bc - 1] + "\n");
+					}
 				}
 			}
 		}
